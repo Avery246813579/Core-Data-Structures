0a9b6dc1c93b9af7f0250393bd4592b2f579b9ac
Avery Durrant <avery246813579@gmail.com>
2017-10-25 19:52:46 -0700

    Finished Linear and Factorial

---
+++ source/recursion.py
@@ -0,0 +1,46 @@
+#!python
+
+def factorial(n):
+    """factorial(n) returns the product of the integers 1 through n for n >= 0,
+    otherwise raises ValueError for n < 0 or non-integer n"""
+    # check if n is negative or not an integer (invalid input)
+    if n < 0 or not isinstance(n, int):
+        raise ValueError('factorial is undefined for n = {}'.format(n))
+    # implement factorial_iterative and factorial_recursive below, then
+    # change this to call your implementation to verify it passes all tests
+    return factorial_iterative(n)
+    # return factorial_recursive(n)
+
+def factorial_iterative(n):
+    if n == 0 or n == 1:
+        return 1
+
+    fact = n
+    for i in range(n - 1, 0, -1):
+        fact *= i
+
+    return fact
+
+def factorial_recursive(n):
+    # check if n is one of the base cases
+    if n == 0 or n == 1:
+        return 1
+    # check if n is an integer larger than the base cases
+    elif n > 1:
+        # call function recursively
+        return n * factorial_recursive(n - 1)
+
+
+def main():
+    import sys
+    args = sys.argv[1:]  # Ignore script file name
+    if len(args) == 1:
+        num = int(args[0])
+        result = factorial(num)
+        print('factorial({}) => {}'.format(num, result))
+    else:
+        print('Usage: {} number'.format(sys.argv[0]))
+
+
+# if __name__ == '__main__':
+#     main()
---
+++ source/search.py
@@ -0,0 +1,50 @@
+#!python
+
+def linear_search(array, item):
+    """return the first index of item in array or None if item is not found"""
+    # implement linear_search_iterative and linear_search_recursive below, then
+    # change this to call your implementation to verify it passes all tests
+    return linear_search_iterative(array, item)
+    # return linear_search_recursive(array, item)
+
+
+def linear_search_iterative(array, item):
+    # loop over all array values until item is found
+    for index, value in enumerate(array):
+        if item == value:
+            return index  # found
+    return None  # not found
+
+
+def linear_search_recursive(array, item, index=0):
+    if index > len(array) - 1:
+        return None
+
+    if array[index] == item:
+        return index
+
+    return linear_search_recursive(array, item, index + 1)
+
+
+def binary_search(array, item):
+    """return the index of item in sorted array or None if item is not found"""
+    # implement binary_search_iterative and binary_search_recursive below, then
+    # change this to call your implementation to verify it passes all tests
+    return binary_search_iterative(array, item)
+    # return binary_search_recursive(array, item)
+
+
+def binary_search_iterative(array, item):
+    # TODO: implement binary search iteratively here
+    pass
+    # once implemented, change binary_search to call binary_search_iterative
+    # to verify that your iterative implementation passes all tests
+
+
+def binary_search_recursive(array, item, left=None, right=None):
+    width = right - left
+
+    if range < 1:
+        return
+
+    ...
