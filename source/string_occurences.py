164e2b471092ec8e69a2a64035c1fd273f7a5ca6
Avery Durrant <avery246813579@gmail.com>
2017-10-29 15:24:35 -0700

    Finished all string occurrences and test cases

--- source/string_occurences.py
+++ source/string_occurences.py
@@ -72,17 +72,64 @@ def index_in_string(text, pattern):
     :param pattern:     The pattern we want to check
     """
 
-    return index_in_string_iterative(text, pattern)
-    # return index_in_string_recursive(text, pattern)
-
+    # return index_in_string_iterative(text, pattern)
+    return index_in_string_recursive(text, pattern)
 
 
 def index_in_string_iterative(text, pattern):
-    pass
+    """ Gets the index of a pattern in a string iteratively """
+
+    text_index = 0
+    pattern_index = 0
+
+    text_length = len(text)
+    pattern_length = len(pattern)
+
+    # Loop through our text characters until we reach the end
+    while text_index < text_length:
+        # If our current text character is equal to our pattern character increase the index
+        if text[text_index] == pattern[pattern_index]:
+            pattern_index += 1
+        # If our current text character is not our pattern character and if we were checking for a pattern, we want to
+        # reset the pattern and try again
+        elif pattern_index > 0:
+            pattern_index = 0
+            continue
+
+        # If our pattern index is equal to our pattern length then we found a match and we can return our current text
+        # index minus our patten length - 1
+        if pattern_index + 1 > pattern_length:
+            return text_index - (pattern_length - 1)
+
+        # Go to next character
+        text_index += 1
+
+    # If we have not found our pattern then we return -1
+    return -1
 
 
 def index_in_string_recursive(text, pattern, text_index=0, pattern_index=0):
-    pass
+    """ Gets the index of a pattern in a string recursively """
+
+    # If our text index is greater then our text then the pattern was not found and we return back -1
+    if text_index + 1 > len(text):
+        return -1
+
+    # If our text character is our pattern character increase our pattern index
+    if text[text_index] == pattern[pattern_index]:
+        pattern_index += 1
+    # If our text character is not our pattern character and our pattern_index is greater then 0 (we were checking) then
+    # lets reset checking from our current index and set our pattern to it's start
+    elif pattern_index > 0:
+        return index_in_string_recursive(text, pattern, text_index, 0)
+
+    # If our pattern index is greater then the len of the pattern then we have a match and we return our text index
+    # minus our (pattern length - 1)
+    if pattern_index + 1 > len(pattern):
+        return text_index - (len(pattern) - 1)
+
+    # Go to the next character in our text string
+    return index_in_string_recursive(text, pattern, text_index + 1, pattern_index)
 
 
 def indexes_in_string(text, pattern):
@@ -92,13 +139,65 @@ def indexes_in_string(text, pattern):
     :param pattern:     The pattern we want to check
     """
 
-    return indexes_in_string_iterative(text, pattern)
     # return indexes_in_string_iterative(text, pattern)
+    return indexes_in_string_recursive(text, pattern)
 
 
 def indexes_in_string_iterative(text, pattern):
-    pass
+    """ Gets our indexes of a pattern from a string iteratively"""
+
+    text_index = 0
+    pattern_index = 0
+
+    text_length = len(text)
+    pattern_length = len(pattern)
+
+    indexes = []
+
+    # Loop through our text characters until we reach the end
+    while text_index < text_length:
+        # If our current text character is equal to our pattern character increase the index
+        if text[text_index] == pattern[pattern_index]:
+            pattern_index += 1
+        # If our current text character is not our pattern character and if we were checking for a pattern, we want to
+        # reset the pattern and try again
+        elif pattern_index > 0:
+            pattern_index = 0
+            continue
+
+        # If our pattern index is equal to our pattern length then we found a match and we want to add the index of the
+        # pattern start to our indexes and reset our pattern index and keep checking
+        if pattern_index + 1 > pattern_length:
+            indexes.append(text_index - (pattern_length - 1))
+            pattern_index = 0
+            continue
+
+        # Go to next character
+        text_index += 1
+
+    # Lets return our indexes
+    return indexes
 
 
-def index_in_string_recursive(text, pattern, text_index=0, pattern_index=0):
-    pass
+def indexes_in_string_recursive(text, pattern, text_index=0, pattern_index=0):
+    # If our text index is greater then our text then the pattern was not found and we return an empty array to append
+    # too
+    if text_index + 1 > len(text):
+        return []
+
+    # If our text character is our pattern character increase our pattern index
+    if text[text_index] == pattern[pattern_index]:
+        pattern_index += 1
+    # If our text character is not our pattern character and our pattern_index is greater then 0 (we were checking) then
+    # lets reset checking from our current index and set our pattern to it's start
+    elif pattern_index > 0:
+        return indexes_in_string_recursive(text, pattern, text_index, 0)
+
+    # If our pattern index is greater then the len of the pattern then we have a match and we return our text index
+    # minus our (pattern length - 1) in an array plus our already created array
+    if pattern_index + 1 > len(pattern):
+        return [text_index - (len(pattern) - 1)] + indexes_in_string_recursive(text, pattern, text_index, 0)
+
+    # Go to the next character in our text string
+    return indexes_in_string_recursive(text, pattern, text_index + 1, pattern_index)
+
--- source/string_occurences_test.py
+++ source/string_occurences_test.py
@@ -16,6 +16,28 @@ class TestPalindromes(unittest.TestCase):
         assert in_string('abcdef', 'k') is False
         assert in_string('abcdef', 'asdfaccs') is False
 
+    def test_index_in_string_is_in(self):
+        assert index_in_string('abcdef', 'abc') == 0
+        assert index_in_string('abcdef', 'def') == 3
+        assert index_in_string('abcdef', 'bcde') == 1
+        assert index_in_string('ababc', 'abc') == 2
+        assert index_in_string("abcdef", 'a') == 0
+        assert index_in_string("abcdef", 'f') == 5
+        assert index_in_string("abcdef", 'c') == 2
+
+    def test_in_string_not_in(self):
+        assert index_in_string('abcdef', 'k') == -1
+        assert index_in_string('abcdef', 'asdfaccs') == -1
+
+    def test_indexes_in_string(self):
+        assert indexes_in_string('ababab', 'ab') == [0, 2, 4]
+        assert indexes_in_string('abcdeabcde', 'abc') == [0, 5]
+        assert indexes_in_string('abababcd', 'aba') == [0, 2]
+        assert indexes_in_string('i123mydog', '123') == [1]
+
+    def test_indexes_in_string_not_in(self):
+        assert indexes_in_string('ababab', 'ds') == []
+        assert indexes_in_string('abcdeabcde', 'dead') == []
 
 if __name__ == '__main__':
     unittest.main()
